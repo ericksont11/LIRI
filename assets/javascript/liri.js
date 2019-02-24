f9d484fc4688b462038fea7ad9ef996291bcc9da
ericksont11 <ericksont91@gmail.com>
2019-02-24 10:33:52 -0500

    added functionality to log to log.txt file

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var axios = require('axios');
 var keys = require("./keys.js");
+var chalk = require('chalk')
 var spotify = new Spotify(keys.spotify);
 var fs = require('fs');
 
@@ -14,6 +15,7 @@ var search = (process.argv.slice(3, process.argv.length)).join("+");
 
 
 function findOnSpotify(search) {
+
     if (search === "") {
         search = "The Sign Ace of Base"
         num = 1
@@ -21,20 +23,38 @@ function findOnSpotify(search) {
     else {
         num = 10
     }
-    console.log("Pulling up songs with the name :" + "'"+search+"'")
+
+    var text = " Pulling up songs with the name :" + "'"+search+"'"
+    fs.appendFile("log.txt", text, function (err) {
+        if (err){
+            console.log("error")
+        }
+        console.log(chalk.yellow(text));
+    });
     spotify.search({ type: "track", query: search, limit: 10}, function (err, data) {
+  
         if (err) {
             console.log("spotify error: " + err);
         }
         else {
             for (x=0; x < num; x++) {
-            var results = data.tracks.items[x];
-            console.log("\n ____________________________\n")
-            console.log(" Name: " + results.name);
-            console.log(" Artist: " + results.artists[0].name); 
-            console.log(" Album: " + results.album.name);
-            console.log(" Preview " + results.preview_url)
+                var results = data.tracks.items[x];
+                var info = [
+                "\n ____________________________\n",
+                " Name: " + results.name,
+                " Artist: " + results.artists[0].name,
+                " Album: " + results.album.name,
+                " Preview: " + results.preview_url
+                ].join("\n")
+                
+                console.log(chalk.gray(info))
             }
+
+            fs.appendFile("log.txt", info, function (err) {
+                if (err){
+                    console.log("error")
+                }
+            });
         }
     });
 };
@@ -43,19 +63,37 @@ function movieInfo(search) {
     if (search === "") {
         search = "Mr.Nobody"
     }
-    console.log("\n Pulling up information about the film: " + "'"+ search)
+   
+    var text = "\n\n\n Pulling up information about the film: " + "'"+ search +"'"
+    fs.appendFile("log.txt", text, function (err) {
+        if (err){
+            console.log("error")
+        }
+        console.log(chalk.yellow(text));
+    });
     var queryUrl = "http://www.omdbapi.com/?t=" + search +"&y=&plot=short&apikey=e9f3bbc2";
     axios.get(queryUrl).then(function (response) {
         var results = response.data
-        console.log("\n ____________________________\n")
-        console.log(" Title: " + results.Title);
-        console.log(" Release Year: " + results.Year);
-        console.log(" Imdb Rating: " + results.imdbRating);
-        console.log(" Produced in: " + results.Country);
-        console.log(" Languages: " + results.Language);
-        console.log(" Plot: " + results.Plot);
-        console.log(" Actors: " + results.Actors);
 
+        var info = [
+        "\n ____________________________\n",
+        " Title: " + results.Title,
+        " Release Year: " + results.Year,
+        " Imdb Rating: " + results.imdbRating,
+        " Produced in: " + results.Country,
+        " Languages: " + results.Language,
+        " Plot: " + results.Plot,
+        " Actors: " + results.Actors
+        ].join("\n")
+
+        console.log(chalk.gray(info))
+
+        fs.appendFile("log.txt", info, function (err) {
+            if (err){
+                console.log("error")
+            }
+        });
+        
     });
 }
 
@@ -67,15 +105,32 @@ function concertInfo(search) {
     else {
         num = 10
     }
-    console.log("\n Pulling up tour dates for: " + "'"+ search+ "'")
+    var text = "\n\n\n Pulling up tour dates for: " + "'"+ search+ "'"
+    fs.appendFile("log.txt", text, function (err) {
+        if (err){
+            console.log("error")
+        }
+        console.log(chalk.yellow(text));
+    });
     var queryUrl = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
     axios.get(queryUrl).then(function (response) {
         for (x=0; x < num; x++) {
             var results = response.data[x]
-            console.log("\n ____________________________\n")
-            console.log(" Venue: " + results.venue.name);
-            console.log(" City: " + results.venue.city);
-            console.log(" Date: " + results.datetime);
+            var info = [
+            "\n ____________________________\n",
+            " Venue: " + results.venue.name,
+            " City: " + results.venue.city,
+            " Date: " + results.datetime,
+            ].join("\n")
+
+            console.log(chalk.gray(info));
+
+            fs.appendFile("log.txt", info, function (err) {
+                if (err){
+                    console.log("error")
+                }
+            });
+
         }
 
     });
@@ -123,8 +178,8 @@ else  {
     console.log(" \n Type what category you are searching, followed by the name of the thing you are searching.");
     console.log(" For example:");
     console.log(" ____________________________\n")
-    console.log(" node liri movie Braveheart");
-    console.log(" node liri spotify Braveheart");
-    console.log(" node liri concert Braveheart");
+    console.log(" node liri movie The Departed");
+    console.log(" node liri spotify I want it that way");
+    console.log(" node liri concert Bob Dylan");
 }
 
